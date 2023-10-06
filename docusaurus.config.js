4006bda24c576b76c40121be26eb2ac3603909c0
nanqic <nanqic@163.com>
2023-10-07 07:49:17 +0800

    add: markmap loader

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nanqic/zenote',
+            'https://github.com/nanqic/zenote/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
